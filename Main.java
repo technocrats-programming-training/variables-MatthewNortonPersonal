f6b50126b50d4c55e7ccd2f3e52a58732658d3de
Matmath8 M8 <91503895+MatthewNortonPersonal@users.noreply.github.com>
2021-09-30 01:00:29 +0000

    I added the four line prints from the comments.

--- Main.java
+++ Main.java
@@ -5,19 +5,31 @@ public class Main {
         String lastName = args[1];
         int age = 0;
         try {
-            int age = Integer.parseInt(args[2]);
+            age = Integer.parseInt(args[2]);
         } catch (NumberFormatException ex) {
             ex.printStackTrace();
         }
         
+        
 
         // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
 
+        System.out.println("Hello, I am " + firstName + " " + lastName + " and I am " + age + " years old");
+
         // Print out whether age is even
+        boolean evenAge = age % 2 == 0;
+        
+        System.out.println(evenAge);
 
         // Print out whether firstName equals lastName
 
+        System.out.println(firstName.equals(lastName));
+
         // Print out whether age is greater than 0
+
+        System.out.println(age > 0);
+
+
 
     }
 }
